a4d164e829d27c8266e2ea8d7417472be4fb39d7
nishii3 <nishii3@example.com>
2025-09-06 13:55:53 +0300

    Решение задания 06

--- task_06/solution.py
+++ task_06/solution.py
@@ -7,8 +7,11 @@ def sum_of_digits(number):
     Возвращает сумму цифр числа
     Например, для 123 результат должен быть 1 + 2 + 3 = 6
     """
-    # Ваш код здесь
-    pass
+    ans = 0
+    while number > 0:
+        ans += number % 10
+        number //= 10
+    return ans
 
 
 def count_digits(number):
@@ -16,8 +19,13 @@ def count_digits(number):
     Возвращает количество цифр в числе
     Например, для 123 результат должен быть 3
     """
-    # Ваш код здесь
-    pass
+    ans = 0
+    if number == 0:
+        return 1
+    while number > 0:
+        ans += 1
+        number //= 10
+    return ans
 
 
 def reverse_number(number):
@@ -25,8 +33,12 @@ def reverse_number(number):
     Возвращает число, записанное в обратном порядке
     Например, для 123 результат должен быть 321
     """
-    # Ваш код здесь
-    pass
+    ans = 0
+    while number > 0:
+        ans *= 10
+        ans += number % 10
+        number //= 10
+    return ans 
 
 
 def is_prime(number):
@@ -34,8 +46,14 @@ def is_prime(number):
     Проверяет, является ли число простым
     Простое число - это число больше 1, которое делится только на 1 и на само себя
     """
-    # Ваш код здесь
-    pass
+    i = 2
+    if number <= 1:
+        return False
+    while i * i <= number:
+        if number % i == 0:
+            return False
+        i += 1
+    return True
 
 
 def gcd(a, b):
@@ -43,14 +61,26 @@ def gcd(a, b):
     Находит наибольший общий делитель (НОД) двух чисел
     Используйте алгоритм Евклида
     """
-    # Ваш код здесь
-    pass
-
+    while b != 0:
+        k = b
+        b = a % b
+        a = k
+    return a
 
 def binary_search(sorted_list, target):
     """
     Реализует бинарный поиск элемента в отсортированном списке
     Возвращает индекс элемента, если он найден, иначе -1
     """
-    # Ваш код здесь
-    pass
+    if not sorted_list:
+        return -1
+    l = 0
+    r = len(sorted_list)
+    while r - l > 1:
+        m = (l + r) // 2
+        if sorted_list[m] > target:
+            r = m
+        else:
+            l = m
+    return l if sorted_list[l] == target else -1
+    
