89c3aa77b4b328fc6e60ffcbf0dad35db942539d
nishii3 <nishii3@example.com>
2025-09-10 00:31:51 +0300

    Решение задания 10

--- task_10/solution.py
+++ task_10/solution.py
@@ -6,40 +6,35 @@ def create_set_from_list(lst):
     """
     Создает множество из списка, удаляя дубликаты
     """
-    # Ваш код здесь
-    pass
+    return set(lst)
 
 
 def is_subset(set1, set2):
     """
     Проверяет, является ли set1 подмножеством set2
     """
-    # Ваш код здесь
-    pass
+    return set1.issubset(set2)
 
 
 def union_sets(set1, set2):
     """
     Возвращает объединение двух множеств
     """
-    # Ваш код здесь
-    pass
+    return set1 | set2
 
 
 def intersection_sets(set1, set2):
     """
     Возвращает пересечение двух множеств
     """
-    # Ваш код здесь
-    pass
+    return set1 & set2
 
 
 def difference_sets(set1, set2):
     """
     Возвращает разность множеств (элементы, которые есть в set1, но отсутствуют в set2)
     """
-    # Ваш код здесь
-    pass
+    return set1 - set2
 
 
 def symmetric_difference_sets(set1, set2):
@@ -47,8 +42,7 @@ def symmetric_difference_sets(set1, set2):
     Возвращает симметричную разность множеств
     (элементы, которые есть в одном из множеств, но не в обоих одновременно)
     """
-    # Ваш код здесь
-    pass
+    return union_sets(set1, set2) - intersection_sets(set1, set2)
 
 
 def remove_duplicates_preserve_order(lst):
@@ -56,5 +50,10 @@ def remove_duplicates_preserve_order(lst):
     Удаляет дубликаты из списка, сохраняя порядок первого появления элементов
     Используйте множество для отслеживания уже встреченных элементов
     """
-    # Ваш код здесь
-    pass
+    ans = []
+    n = set()
+    for i in lst:
+        if not i in n:
+            ans.append(i)
+            n.add(i)
+    return ans
